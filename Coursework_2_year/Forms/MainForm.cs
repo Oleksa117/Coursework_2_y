bd798f098b42187b1a7d708cc13d435e65c980f6
Олександра Лавріненко <oleksandra_lavrinenko@outlook.com>
2026-04-10 22:03:07 +0300

    Add core domain models and initial WinForms project structure

---
+++ Coursework_2_year/Forms/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coursework_2_year.Forms
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            Text = "Concert Ticket System";
+            Width = 800;
+            Height = 600;
+        }
+    }
+}
---
+++ Coursework_2_year/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coursework_2_year.Models
+{
+    internal class Ticket
+    {
+    }
+}
